2ac21d075b7e512bc8ac2c64a6c7fdf6365080e5
Valentin LAY <valentin.lay98@gmail.com>
2022-04-21 20:15:48 +0200

    Update Food flight
    - Ajout de la possibilité de jeter des projectiles.
    - Ajout d'un script permettant de faire bouger les animaux ainsi que les projectiles.
    -Suppression des projectiles et des animaux hors champs.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 13.0f;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +25,13 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Lauch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
